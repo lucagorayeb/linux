229f6f5ebb59f5922f9abc8e2de3e79a4a63a398
Luca Gorayeb <lucagorayeb@gmail.com>
2026-05-08 21:16:45 -0400

    updating

---
+++ linux_bible/shell_script_7/parametros_posicionais.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script to echo out command-line arguments
+echo "The first argument is $1, the second is $2."
+echo "The command itself is called $0."
+echo "A quantidade de parametros foi de $#."
+echo "Os argumentos passados foram $@."
+echo "O status de saída foi $?."
